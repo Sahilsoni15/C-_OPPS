9dfbd7bdddaf0f851e40f6b0ebc4461cd5e79dee
Sahilsoni15 <sahil157soni@gmail.com>
2025-11-16 14:26:37 +0530

    constructor overloading in the 31.cpp

--- OOPS/31.cpp
+++ OOPS/31.cpp
@@ -1,34 +1,41 @@
 // CONSTRUCTOR OVERLOARDING
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Complex{
-    int a,b;
-    public:
-    //DEFAULT CONSTRUCTOR
-    Complex(){
-        a=0;
-        b=0;
+class Complex
+{
+    int a, b;
+
+public:
+    // DEFAULT CONSTRUCTOR
+    Complex()
+    {
+        a = 0;
+        b = 0;
     }
 
-    //PARAMETERIZED CONSTRUCTOR 
-    Complex(int x,int y){
-        a=x;
-        b=y;
+    // PARAMETERIZED CONSTRUCTOR
+    Complex(int x, int y)
+    {
+        a = x;
+        b = y;
     }
-    //PARAMETERIZED CONSTRUCTOR
-    Complex(int x){
-        a=x;
-        b=0;
+    // PARAMETERIZED CONSTRUCTOR
+    Complex(int x)
+    {
+        a = x;
+        b = 0;
     }
 
-    void printnumber(){
-        cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+    void printnumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
     }
 };
 
-int main(){
-    Complex c1(4,6);
+int main()
+{
+    Complex c1(4, 8);
     c1.printnumber();
 
     Complex c2(7);
