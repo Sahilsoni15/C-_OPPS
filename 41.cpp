c2ffbc9f341375b2e304b0ebaea03f144955dae5
Sahilsoni15 <sahil157soni@gmail.com>
2025-12-04 19:56:23 +0530

    Multilevel and Multiple Inheritance

---
+++ 41.cpp
@@ -0,0 +1,65 @@
+//MULTIPLE INHERITANCE
+/*
+SYNTAX-->
+// class DerivedC: visibility-mode base1, visibility-mode base2
+// {
+//      Class body of class "DerivedC"
+// };
+*/
+
+#include<iostream>
+using namespace std;
+
+class Base1{
+protected:
+    int base1int;
+
+public:
+    void set_base1int(int a)
+    {
+        base1int = a;
+    }
+};
+
+class Base2{
+protected:
+    int base2int;
+
+public:
+    void set_base2int(int a)
+    {
+        base2int = a;
+    }
+};
+
+class Base3{
+protected:
+    int base3int;
+
+public:
+    void set_base3int(int a)
+    {
+        base3int = a;
+    }
+};
+class Derived : public Base1, public Base2, public Base3
+{
+    public: 
+        void show(){
+            cout << "The value of Base1 is " << base1int<<endl;
+            cout << "The value of Base2 is " << base2int<<endl;
+            cout << "The value of Base3 is " << base3int<<endl;
+            cout << "The sum of these values is " << base1int + base2int + base3int << endl;
+        }
+};
+
+int main()
+{
+    Derived sahil;
+    sahil.set_base1int(25);
+    sahil.set_base2int(5);
+    sahil.set_base3int(15);
+    sahil.show();
+    
+    return 0;
+}
---
+++ OOPS/39.cpp
@@ -0,0 +1,26 @@
+//Protected
+#include<iostream>
+using namespace std;
+class Base{
+    protected:
+        int a;
+    private:
+        int b;
+};
+
+/*
+For a protected member:
+                            Public Derivation       Private Derivation      Protected Derivation
+    1.Private members          Not Inheritated          Not Inheritated         Not Inheritated       
+    2.Protected members            Protected                Private                 Protected
+    3.Public members               Public                   Private                 Protected
+*/
+class Derived : protected Base{
+
+};
+int main(){
+    Base b();
+    Derived d();
+    // cout<<d.a; //will not work since a is protected in both base as well as derived class
+    return 0;
+}
