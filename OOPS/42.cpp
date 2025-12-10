c2518cb61b9f3c5fcb36ed9aa0132db06cbf06ba
Sahilsoni15 <sahil157soni@gmail.com>
2025-12-10 20:01:43 +0530

    Question

--- OOPS/42.cpp
+++ OOPS/42.cpp
@@ -26,56 +26,112 @@ Create 2 classes:
         -->Protected p,q shared across all 3 classes → single data storage
         -->Virtual inheritance eliminates diamond problem → clean access
 */
-class SimpleCalculator{
-    protected:
-        int p;
-        int q;
+
+class SimpleCalculator {
+    int a, b;
     public:
-        void input(){
-            cout<<"Enter a 1st number: ";
-            cin>>p;
-            cout<<endl;
-            cout<<"Enter a 2nd number: ";
-            cin>>q;
-            cout<<endl;
+        void getDataSimple()
+        {
+            cout<<"Enter the value of a"<<endl;
+            cin>>a;
+            cout<<"Enter the value of b"<<endl;
+            cin>>b;
         }
 
-        void output(){
-            cout<<"----Simple Calculator----";
-            cout<<endl;
-            cout<<"Addition is "<<p+q<<endl;
-            cout<<"Subtraction is "<<p-q<<endl;
-            cout<<"Multiplication is "<<p*q<<endl;
-            cout<<"Division is "<<p/q<<endl;
+        void performOperationsSimple(){
+            cout<<"The value of a + b is: "<<a + b<<endl;
+            cout<<"The value of a - b is: "<<a - b<<endl;
+            cout<<"The value of a * b is: "<<a * b<<endl;
+            cout<<"The value of a / b is: "<<a / b<<endl;
         }
 };
 
-class ScientificCalculator:virtual public SimpleCalculator{
+class ScientificCalculator{
+    int a, b;
+
     public:
-        void scientific(){
-            cout<<endl;
-            cout<<"----Scientific Calculator----";
-            cout<<endl;
-            cout<<"Square root of p is "<<sqrt(p)<<endl;
-            cout<<"Cuberoot of p is "<<cbrt(p)<<endl;
-			cout<<"Square root of q is "<<sqrt(q)<<endl;
-			cout<<"Cuberoot of q is "<<cbrt(q)<<endl;
-			cout<<"p to the power q is "<<pow(p,q)<<endl;
+        void getDataScientific()
+        {
+            cout << "Enter the value of a" << endl;
+            cin >> a;
+            cout << "Enter the value of b" << endl;
+            cin >> b;
+        }
+
+        void performOperationsScientific()
+        { 
+            cout << "The value of cos(a) is: " << cos(a) << endl;
+            cout << "The value of sin(a) is: " << sin(a) << endl;
+            cout << "The value of exp(a) is: " << exp(a) << endl;
+            cout << "The value of tan(a) is: " << tan(a) << endl;
         }
 };
 
-class HybridCalculator : virtual public SimpleCalculator,public ScientificCalculator{
-    public:
-        void demonstrateall(){
-            input();
-
-            output();
-            scientific();
-        }
+class HybridCalculator : public SimpleCalculator, public ScientificCalculator{
+    
 };
 
-int main(){
-    HybridCalculator calcu;
-    calcu.demonstrateall();
+int main()
+{
+    HybridCalculator calc;
+    calc.getDataScientific();
+    calc.performOperationsScientific();
+    calc.getDataSimple();
+    calc.performOperationsSimple();
+    
     return 0;
 }
+
+// class SimpleCalculator{
+//     protected:
+//         int p;
+//         int q;
+//     public:
+//         void input(){
+//             cout<<"Enter a 1st number: ";
+//             cin>>p;
+//             cout<<endl;
+//             cout<<"Enter a 2nd number: ";
+//             cin>>q;
+//             cout<<endl;
+//         }
+
+//         void output(){
+//             cout<<"----Simple Calculator----";
+//             cout<<endl;
+//             cout<<"Addition is "<<p+q<<endl;
+//             cout<<"Subtraction is "<<p-q<<endl;
+//             cout<<"Multiplication is "<<p*q<<endl;
+//             cout<<"Division is "<<p/q<<endl;
+//         }
+// };
+
+// class ScientificCalculator:virtual public SimpleCalculator{
+//     public:
+//         void scientific(){
+//             cout<<endl;
+//             cout<<"----Scientific Calculator----";
+//             cout<<endl;
+//             cout<<"Square root of p is "<<sqrt(p)<<endl;
+//             cout<<"Cuberoot of p is "<<cbrt(p)<<endl;
+// 			cout<<"Square root of q is "<<sqrt(q)<<endl;
+// 			cout<<"Cuberoot of q is "<<cbrt(q)<<endl;
+// 			cout<<"p to the power q is "<<pow(p,q)<<endl;
+//         }
+// };
+
+// class HybridCalculator : virtual public SimpleCalculator,public ScientificCalculator{
+//     public:
+//         void demonstrateall(){
+//             input();
+
+//             output();
+//             scientific();
+//         }
+// };
+
+// int main(){
+//     HybridCalculator calcu;
+//     calcu.demonstrateall();
+//     return 0;
+// }
